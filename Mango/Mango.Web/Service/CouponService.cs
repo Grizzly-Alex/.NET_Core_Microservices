0b0980bdf9f3bc66625ceaced9d1043504513163
Alexander Medved <medved1991alexander@gmail.com>
2023-12-20 17:42:48 +0300

    Register Services in Program Class File

---
+++ Mango/Mango.Web/Service/CouponService.cs
@@ -0,0 +1,45 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+
+namespace Mango.Web.Service
+{
+    public sealed class CouponService : ICouponService
+    {
+        private readonly IBaseService _baseService;
+
+        public CouponService(IBaseService baseService)
+        {
+            _baseService = baseService;
+        }
+
+        public Task<ResponseDto> CreateCouponAsync(CouponDto coupon)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto> DeleteCouponAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ResponseDto>> GetAllCouponsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto> GetCouponByCodeAsync(string couponCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto> GetCouponByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto> UpdateCouponAsync(CouponDto coupon)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Mango/Mango.Web/Utility/SD.cs
+++ Mango/Mango.Web/Utility/SD.cs
@@ -2,6 +2,7 @@
 {
     public sealed class SD
     {
+        public static string CouponAPIBase {  get; set; }
         public enum ApiType
         {
             GET,
