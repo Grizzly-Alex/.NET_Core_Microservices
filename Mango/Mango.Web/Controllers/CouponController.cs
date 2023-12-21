60d475efa83e1755d2dae30d4b0898d61a1a1de1
Alexander Medved <medved1991alexander@gmail.com>
2023-12-21 10:55:56 +0300

    Coupon Controller

---
+++ Mango/Mango.Web/Controllers/CouponController.cs
@@ -0,0 +1,28 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Mango.Web.Controllers
+{
+    public class CouponController : Controller
+    {
+        private readonly ICouponService _couponService;
+
+        public CouponController(ICouponService couponService)
+        {
+            _couponService = couponService;              
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            ResponseDto response = await _couponService.GetAllCouponsAsync();
+
+            var list = response != null && response.IsSuccess
+                ? JsonConvert.DeserializeObject<List<CouponDto>>(Convert.ToString(response.Result))
+                : new List<CouponDto>();
+
+            return View(list);
+        }
+    }
+}
