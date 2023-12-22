b7f2b6193b423afda0dc7a27647c4f2796f61ea0
Alexander Medved <medved1991alexander@gmail.com>
2023-12-22 15:04:30 +0300

    Add Mapping

---
+++ Mango/Mango.Services.AuthAPI/MappingConfig.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Mango.Services.AuthAPI.Models;
+using Mango.Services.AuthAPI.Models.Dto;
+
+namespace Mango.Services.AuthAPI
+{
+    public sealed class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            return new MapperConfiguration(config =>
+            {
+                config.CreateMap<AppUser, RegistrationRequestDto>()
+                    .ForMember(dest => dest.Email, act => act.MapFrom(src => src.UserName))
+                    .ForMember(dest => dest.Email, act => act.MapFrom(src => src.Email))
+                    .ForMember(dest => dest.Email.ToLower(), act => act.MapFrom(src => src.NormalizedEmail))
+                    .ForMember(dest => dest.Name, act => act.MapFrom(src => src.Name))
+                    .ForMember(dest => dest.PhoneNumber, act => act.MapFrom(src => src.PhoneNumber))
+                    .ReverseMap();
+
+                config.CreateMap<AppUser, UserDto>()
+                    .ForMember(dest => dest.ID, act => act.MapFrom(src => src.Id))
+                    .ForMember(dest => dest.Email, act => act.MapFrom(src => src.Email))
+                    .ForMember(dest => dest.Name, act => act.MapFrom(src => src.Name))
+                    .ForMember(dest => dest.PhoneNumber, act => act.MapFrom(src => src.PhoneNumber))
+                    .ReverseMap();
+
+            });
+        }
+    }
+}
--- Mango/Mango.Services.AuthAPI/Services/AuthService.cs
+++ Mango/Mango.Services.AuthAPI/Services/AuthService.cs
@@ -1,4 +1,5 @@
-﻿using Mango.Services.AuthAPI.Data;
+﻿using AutoMapper;
+using Mango.Services.AuthAPI.Data;
 using Mango.Services.AuthAPI.Models;
 using Mango.Services.AuthAPI.Models.Dto;
 using Mango.Services.AuthAPI.Services.IService;
@@ -11,15 +12,18 @@ namespace Mango.Services.AuthAPI.Services
         private readonly AppDbContext _db;
         private readonly UserManager<AppUser> _userManager;
         private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IMapper _mapper;
 
         public AuthService(
             AppDbContext db,
             UserManager<AppUser> userManager,
-            RoleManager<IdentityRole> roleManager)
+            RoleManager<IdentityRole> roleManager,
+            IMapper mapper)
         {
             _db = db;    
             _userManager = userManager;
             _roleManager = roleManager;
+            _mapper = mapper;   
         }
 
         public async Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto)
@@ -29,7 +33,24 @@ namespace Mango.Services.AuthAPI.Services
 
         public async Task<UserDto> Register(RegistrationRequestDto registrationRequestDto)
         {
-            throw new NotImplementedException();
+            AppUser user = _mapper.Map<AppUser>(registrationRequestDto);
+
+            try
+            {
+                var result = await _userManager.CreateAsync(user, registrationRequestDto.Password);
+
+                if (result.Succeeded)
+                {
+                    var userToReturn = _db.Users.First(user => user.UserName == registrationRequestDto.Email);
+
+                    return _mapper.Map<UserDto>(userToReturn);
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return new();
         }
     }
 }
