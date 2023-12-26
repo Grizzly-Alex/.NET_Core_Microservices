00dc7a48de236161aa512f15f14b92c681cd5176
Alexander Medved <medved1991alexander@gmail.com>
2023-12-26 17:43:25 +0300

    Add DTO's in Web Project

---
+++ Mango/Mango.Web/Models/LoginRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Mango.Web.Models
+{
+    public sealed class LoginRequestDto
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
--- Mango/Mango.Web/Utility/SD.cs
+++ Mango/Mango.Web/Utility/SD.cs
@@ -2,6 +2,7 @@
 {
     public sealed class SD
     {
+        public static string AuthAPIBase { get; set; }
         public static string CouponAPIBase {  get; set; }
         public enum ApiType
         {
