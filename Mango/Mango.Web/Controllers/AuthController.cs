2c6e250e7f4dd0d2210556d500f359f73333ba40
Alexander Medved <medved1991alexander@gmail.com>
2023-12-27 12:22:34 +0300

    Add dropdown for Role

--- Mango/Mango.Web/Controllers/AuthController.cs
+++ Mango/Mango.Web/Controllers/AuthController.cs
@@ -1,6 +1,8 @@
 ﻿using Mango.Web.Models;
 using Mango.Web.Service.IService;
+using Mango.Web.Utility;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Mango.Web.Controllers
 {
@@ -17,7 +19,18 @@ namespace Mango.Web.Controllers
         public IActionResult Login() => View(new LoginRequestDto());
 
         [HttpGet]
-        public IActionResult Register() => View(new RegistrationRequestDto());
+        public IActionResult Register() 
+        {
+            var roleList = new List<SelectListItem>()
+            {
+                new() { Value = SD.Role.Admin.ToString(), Text = SD.Role.Admin.ToString() },
+                new() { Value = SD.Role.Customer.ToString(), Text = SD.Role.Customer.ToString() },
+            };
+
+            ViewBag.RoleList = roleList;
+
+            return View();
+        }
 
         [HttpPost]
         public IActionResult Logout() 
--- Mango/Mango.Web/Utility/SD.cs
+++ Mango/Mango.Web/Utility/SD.cs
@@ -2,8 +2,17 @@
 {
     public sealed class SD
     {
+        #region API bases
         public static string AuthAPIBase { get; set; }
         public static string CouponAPIBase {  get; set; }
+        #endregion
+
+        public enum Role
+        {
+            Admin,
+            Customer,
+        }
+
         public enum ApiType
         {
             GET,
