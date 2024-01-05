95dc6064457dc6445ac2f9b4f334a48ac44773da
Alexander Medved <medved1991alexander@gmail.com>
2024-01-05 16:42:14 +0300

    Add mapper to product api

---
+++ Mango/Mango.Services.ProductAPI/MappingConfig.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Mango.Services.ProductAPI.Models;
+using Mango.Services.ProductAPI.Models.Dto;
+
+namespace Mango.Services.ProductAPI
+{
+    public sealed class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            return new MapperConfiguration(config =>
+            {
+                config.CreateMap<ProductDto, Product>().ReverseMap();
+            });
+        }
+    }
+}
--- Mango/Mango.Services.ProductAPI/Program.cs
+++ Mango/Mango.Services.ProductAPI/Program.cs
@@ -1,3 +1,5 @@
+using AutoMapper;
+using Mango.Services.ProductAPI;
 using Mango.Services.ProductAPI.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,6 +10,9 @@ builder.Services.AddDbContext<AppDbContext>(option =>
 {
     option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
