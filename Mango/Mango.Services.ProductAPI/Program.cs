9a37cb1478545d9265612d1e9eaf61b88bdf8b83
Alexander Medved <medved1991alexander@gmail.com>
2024-01-05 16:35:57 +0300

    Add product to database

--- Mango/Mango.Services.ProductAPI/Program.cs
+++ Mango/Mango.Services.ProductAPI/Program.cs
@@ -1,6 +1,13 @@
+using Mango.Services.ProductAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
