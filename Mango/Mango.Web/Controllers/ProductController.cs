aac5fd1b8dffb6c5c17dcf0a6da95d811693f59a
Alexander Medved <medved1991alexander@gmail.com>
2024-01-08 17:58:30 +0300

    Product controller

---
+++ Mango/Mango.Web/Controllers/ProductController.cs
@@ -0,0 +1,99 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+
+namespace Mango.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;              
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<ProductDto>? list = new();
+
+            var response = await _productService.GetAllProductsAsync();
+
+            if (response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(response.Result));
+            }
+            else
+            {
+                TempData["error"] = response?.Message;
+            }
+
+            return View(list);
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ProductDto model)
+        {
+            if(ModelState.IsValid)
+            {
+                var response = await _productService.CreateProductAsync(model);
+
+                if (response != null && response.IsSuccess)
+                {
+                    TempData["success"] = "Product created successfully";
+
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    TempData["error"] = response?.Message;
+                }
+            }
+            return View(model);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _productService.GetProductByIdAsync(id);
+
+                if (response != null && response.IsSuccess)
+                {
+                    var model = JsonConvert.DeserializeObject<ProductDto>(Convert.ToString(response.Result));
+
+                    return View(model);
+                }
+                else
+                {
+                    TempData["error"] = response?.Message;
+                }
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(ProductDto model)
+        {            
+            var response = await _productService.DeleteProductAsync(model.Id);
+
+            if (response != null && response.IsSuccess)
+            {
+                TempData["success"] = "Product deleted successfully";
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                TempData["error"] = response?.Message;
+            }
+            return View(model);
+        }
+    }
+}
