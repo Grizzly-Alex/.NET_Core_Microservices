3128ed5d3ebf001db78369b7ef7185e7af5d6284
Alexander Medved <medved1991alexander@gmail.com>
2024-01-09 12:15:36 +0300

    Add product details UI

--- Mango/Mango.Web/Models/ProductDto.cs
+++ Mango/Mango.Web/Models/ProductDto.cs
@@ -1,4 +1,6 @@
-﻿namespace Mango.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Web.Models
 {
     public sealed class ProductDto
     {
@@ -7,6 +9,8 @@
         public double Price { get; set; }
         public string Description { get; set; }
         public string Category { get; set; }
-        public string ImageUrl { get; set; }
+        public string? ImageUrl { get; set; }
+        [Range(1, 100)]
+        public int Count { get; set; } = 1;
     }
 }
