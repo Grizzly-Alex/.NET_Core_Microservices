a7a8e7ac32bed7d44a3121d3105ef5030753522c
Alexander Medved <medved1991alexander@gmail.com>
2024-01-12 16:47:19 +0300

    Load Shopping Cart

---
+++ Mango/Mango.Web/Controllers/CartController.cs
@@ -0,0 +1,34 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.IdentityModel.Tokens.Jwt;
+using System.Reflection;
+
+namespace Mango.Web.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ICartService _cartService;
+
+        public CartController(ICartService couponService)
+        {
+            _cartService = couponService;              
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Index() => View(await LoadCartDtoBasedOnLoggedInUser());
+
+
+        private async Task<CartDto> LoadCartDtoBasedOnLoggedInUser()
+        {
+            var userId = User.Claims.Where(user => user.Type == JwtRegisteredClaimNames.Sub)?.FirstOrDefault()?.Value;
+            ResponseDto? response = await _cartService.GetCartByUserIdAsync(userId);
+
+            return response != null & response.IsSuccess 
+                ? JsonConvert.DeserializeObject<CartDto>(Convert.ToString(response)) 
+                : new CartDto();
+        }
+    }
+}
