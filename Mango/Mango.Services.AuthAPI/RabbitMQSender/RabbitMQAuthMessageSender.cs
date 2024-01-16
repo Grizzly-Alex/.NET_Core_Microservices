e93b69039852f6e0f0e3f32082d69947aead4289
Alexander Medved <medved1991alexander@gmail.com>
2024-01-16 17:31:08 +0300

    RabbitMQ message sender

---
+++ Mango/Mango.Services.AuthAPI/RabbitMQSender/RabbitMQAuthMessageSender.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace Mango.Services.AuthAPI.RabbitMQSender
+{
+    public sealed class RabbitMQAuthMessageSender : IRabbitMQAuthMessageSender
+    {
+        private readonly string _hostName;
+        private readonly string _userName;
+        private readonly string _password;
+        private IConnection _connection;
+
+        public RabbitMQAuthMessageSender()
+        {
+            _hostName = "localhost";
+            _userName = "guest";
+            _password = "guest";             
+        }
+
+        public void SendMessage(object message, string queueName)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = _hostName,
+                UserName = _userName,
+                Password = _password,
+            };
+
+            _connection = factory.CreateConnection();
+
+            using var channel = _connection.CreateChannel();
+            channel.QueueDeclare(queueName);
+
+            var json = JsonConvert.SerializeObject(message);
+            var body = Encoding.UTF8.GetBytes(json);
+
+            channel.BasicPublish(exchange: string.Empty, routingKey: queueName, body: body);
+        }
+    }
+}
