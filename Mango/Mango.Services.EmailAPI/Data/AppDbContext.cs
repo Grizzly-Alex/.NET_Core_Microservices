f628e404e2633084cee74f5d0f7931812cf00113
Alexander Medved <medved1991alexander@gmail.com>
2024-01-19 10:54:39 +0300

    Setup email and Dto's q w

---
+++ Mango/Mango.Services.EmailAPI/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Mango.Services.EmailAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.EmailAPI.Data
+{
+    public sealed class AppDbContext : DbContext
+    {
+        public  DbSet<EmailLogger> EmailLoggers { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+    }
+}
