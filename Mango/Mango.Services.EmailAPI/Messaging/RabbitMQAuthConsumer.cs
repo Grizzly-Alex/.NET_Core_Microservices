726cec5b4140b25c651bce9242bac1ad324177f9
Alexander Medved <medved1991alexander@gmail.com>
2024-01-23 09:44:52 +0300

    Create rabbitMQ Consummer part2

---
+++ Mango/Mango.Services.EmailAPI/Messaging/RabbitMQAuthConsumer.cs
@@ -0,0 +1,54 @@
+﻿using Mango.EmailAPI.Utility;
+using Mango.Services.EmailAPI.Services;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace Mango.Services.EmailAPI.Messaging
+{
+    public class RabbitMQAuthConsumer : BackgroundService
+    {   
+        private readonly EmailService _emailService;
+        private readonly IConnection _connection;
+        private IModel _channel;
+
+        public RabbitMQAuthConsumer(EmailService emailService)
+        {
+            _emailService = emailService;
+
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                Password = "guest",
+                UserName = "guest",
+            };
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+            _channel.QueueDeclare(SD.RegisterUser, false, false, false, null); ;
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            stoppingToken.ThrowIfCancellationRequested();
+            var consumer = new EventingBasicConsumer(_channel);
+            consumer.Received += (ch, ea) =>
+            {
+                var content = Encoding.UTF8.GetString(ea.Body.ToArray());
+                String email = JsonConvert.DeserializeObject<string>(content);
+                HandleMessage(email).GetAwaiter().GetResult();
+
+                _channel.BasicAck(ea.DeliveryTag, false);
+            };
+
+            _channel.BasicConsume(SD.RegisterUser, false, consumer);
+
+            return Task.CompletedTask;
+        }
+
+        private async Task HandleMessage(string email)
+        {
+
+        }
+    }
+}
---
+++ Mango/Mango.Services.EmailAPI/Utility/SD.cs
@@ -0,0 +1,10 @@
+﻿namespace Mango.EmailAPI.Utility
+{
+    public sealed class SD
+    {
+        public static string EmailShoppingCart { get; set; }
+        public static string RegisterUser { get; set; }
+        public static string OrderCreated { get; set; }
+        public static string OrderCreatedEmail { get; set; }
+    }
+}
