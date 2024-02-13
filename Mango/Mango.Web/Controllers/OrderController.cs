bd223d22a5495bc9128bad7d31a781c97d881f63
Alexander Medved <medved1991alexander@gmail.com>
2024-02-13 09:36:38 +0300

    configure database endpoint

--- Mango/Mango.Web/Controllers/OrderController.cs
+++ Mango/Mango.Web/Controllers/OrderController.cs
@@ -1,12 +1,48 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Mango.Web.Utility;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.IdentityModel.Tokens.Jwt;
+
 
 namespace Mango.Web.Controllers
 {
     public class OrderController : Controller
     {
+        private readonly IOrderService _orderService;
+
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;               
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult GetAll() 
+        {
+            IEnumerable<OrderHeaderDto> list;
+            string userId = string.Empty;
+
+            if (!User.IsInRole(SD.RoleAdmin))
+            {
+                userId = User.Claims.Where(user => user.Type == JwtRegisteredClaimNames.Sub)?.FirstOrDefault()?.Value;
+            }
+            ResponseDto response = _orderService.GetAllOrdersAsync(userId).GetAwaiter().GetResult();
+
+            if (response != null)
+            {
+                list = JsonConvert.DeserializeObject<List<OrderHeaderDto>>(Convert.ToString(response.Result));
+            }
+            else
+            {
+                list = new List<OrderHeaderDto>();  
+            }
+            return Json(new {data = list });
         }
     }
 }
---
+++ Mango/Mango.Web/wwwroot/js/orderTable.js
@@ -0,0 +1,19 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url: "/order/getAll" },
+        "columns": [
+            { data: 'id', "width": "5%" },
+            { data: 'email', "width": "25%" },
+            { data: 'name', "width": "20%" },
+            { data: 'phone', "width": "10%" },
+            { data: 'status', "width": "10%" },
+            { data: 'total', "width": "10%" },
+        ]
+    })
+}
