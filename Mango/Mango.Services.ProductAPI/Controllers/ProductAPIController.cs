4318603e2f42dd43d5cc7ce560d95fd7393c4c01
Alexander Medved <medved1991alexander@gmail.com>
2024-02-14 15:19:29 +0300

    Solve bug

--- Mango/Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
+++ Mango/Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -76,7 +76,7 @@ namespace Mango.Services.ProductAPI.Controllers
                         productDto.Image.CopyTo(fileStream);
                     }
                     var baseUrl = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host.Value}{HttpContext.Request.PathBase.Value}";
-                    product.ImageUrl = $"{baseUrl}/ProductImages/{filePath}";
+                    product.ImageUrl = $"{baseUrl}/ProductImages/{fileName}";
                     product.ImageLocalPath = filePath;
                 }
                 else
