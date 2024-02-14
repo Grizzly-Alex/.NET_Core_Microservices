ce15db2bf1b82ca2d9e0b1e0aecbf986080ddd0f
Alexander Medved <medved1991alexander@gmail.com>
2024-02-14 17:01:05 +0300

    Add AllowedExtensionsAttribute

---
+++ Mango/Mango.Web/Utility/AllowedExtensionsAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Web.Utility
+{
+    public class AllowedExtensionsAttribute : ValidationAttribute
+    {
+        private readonly string[] _extensions;
+
+        public AllowedExtensionsAttribute(string[] extenstions)
+        {
+            _extensions = extenstions;    
+        }
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            var file = value as IFormFile;
+
+            if (file != null)
+            {
+                var extension = Path.GetExtension(file.FileName);
+                if (!_extensions.Contains(extension.ToLower()))
+                {
+                    return new ValidationResult("This photo extension is not allowed!");
+                }
+            }
+
+            return base.IsValid(value, validationContext);
+        }
+    }
+}
