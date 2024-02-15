885fb9387e71f268272255c84e60d0e81f91cec2
Alexander Medved <medved1991alexander@gmail.com>
2024-02-15 15:55:32 +0300

    Add Autentication to Ocelot

---
+++ Mango/Mango.GatewaySolution/Extensions/WebApplicationBuilderExtencions.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Mango.GatewaySolution.Extensions
+{
+    public static class WebApplicationBuilderExtencions
+    {
+        public static WebApplicationBuilder AddAppAuthetication(this WebApplicationBuilder builder)
+        {
+            var setingsSection = builder.Configuration.GetSection("ApiSettings");
+            var secret = setingsSection.GetValue<string>("Secret");
+            var issuer = setingsSection.GetValue<string>("Issuer");
+            var audience = setingsSection.GetValue<string>("Audience");
+
+            var key = Encoding.ASCII.GetBytes(secret);
+
+            builder.Services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidIssuer = issuer,
+                    ValidAudience = audience,
+                };
+            });
+
+            return builder;
+        }
+    }
+}
--- Mango/Mango.GatewaySolution/Program.cs
+++ Mango/Mango.GatewaySolution/Program.cs
@@ -1,9 +1,13 @@
+using Mango.GatewaySolution.Extensions;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+builder.AddAppAuthetication();
 builder.Services.AddOcelot();
+
+
+var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 app.UseOcelot();
